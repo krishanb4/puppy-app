138d1d5a0dc22d524b03938aba9e6a402aea0fef
crezix <52518140+crezix@users.noreply.github.com>
2021-05-12 19:07:16 +0530

    loyalty

---
+++ src/functions/pinataFunc.js
@@ -0,0 +1,40 @@
+const pinataSDK = require('@pinata/sdk');
+const pinata = pinataSDK(
+  'fcebc7750ffb180096f2',
+  'e5f7b75d4870a713863320ea1e6560ceb0d2bfc8873e7cb7633232c874e58739'
+);
+const collectibles = require('../constants/constants').royaltyCollectibles;
+
+async function generateItem(id, name, description, external_url, address) {
+  const body = {
+    description: collectibles[`${id}`].description,
+    external_url: 'https://puppy.finance/nft/multi/' + id,
+    image: collectibles[`${id}`].image,
+    name: collectibles[`${id}`].name,
+    tokenId: id,
+    attributes: [
+      {
+        display_type: 'date',
+        trait_type: 'birthday',
+        value: Date.now(),
+      },
+    ],
+  };
+
+  const options = {
+    pinataMetadata: {
+      name: `${id}:${address}`,
+      keyvalues: {
+        name: `${id}:${address}`,
+      },
+    },
+    pinataOptions: {
+      cidVersion: 0,
+    },
+  };
+
+  const data = await pinata.pinJSONToIPFS(body, options);
+  return data;
+}
+
+export {generateItem};
--- src/pages/Loyalty.js
+++ src/pages/Loyalty.js
@@ -1,34 +1,99 @@
 import React from 'react';
-import {Container, Row, Col, Carousel} from 'react-bootstrap';
+import {Container, Row, Col, Button, Card} from 'react-bootstrap';
 import logo from '.././images/main.png';
+import puppy from '../puppy.png';
+import {generateItem} from '../functions/pinataFunc';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 class Loyalty extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      balance: 100.24,
+      level: 3,
+    };
+    this.checkLevel = this.checkLevel.bind(this);
+    this.generate = this.generate.bind(this);
+  }
+
+  async checkLevel() {
+    const level = 4;
+    this.setState({level: this.state.level + 1});
+    return level;
+  }
+
+  async generate() {
+    const level = await this.checkLevel();
+    const ipfs_res = await generateItem(level, '', '', '', '');
+    const hash = ipfs_res.IpfsHash;
+    console.log(hash);
+  }
+  componentDidMount() {
+    this.checkLevel();
+  }
+
   render() {
-    const items = [];
+    const responsive = {
+      superLargeDesktop: {
+        // the naming can be any, depends on you.
+        breakpoint: {max: 4000, min: 3000},
+        items: 5,
+      },
+      desktop: {
+        breakpoint: {max: 3000, min: 1024},
+        items: 4,
+      },
+      tablet: {
+        breakpoint: {max: 1024, min: 464},
+        items: 2,
+      },
+      mobile: {
+        breakpoint: {max: 464, min: 0},
+        items: 1,
+      },
+    };
     for (var i = 0; i < 5; i++) {
-      items.push(
-        <Carousel.Item>
-          <img
-            className="d-block w-10"
-            src="https://picsum.photos/500/300?img=1"
-            alt="First slide"
-          />
-          <Carousel.Caption></Carousel.Caption>
-        </Carousel.Item>
-      );
+      this.state.items.push(<img width={100} className="mr-3" src={puppy} />);
     }
     return (
-      <div>
+      <div className="sm puppy-head">
         <Row>
+          <Col>
+            <Button variant="outline-light" onClick={this.generate}>
+              Claim NFT
+            </Button>
+          </Col>
+        </Row>
+        <h3>What is Loyalty?</h3>
+        <p>
+          There are levels to all PUPPY Holders. You can claim NFTs matching
+          your level!
+        </p>
+
+        <Row>
+          <Col></Col>
           <Col sm>
-            <h3>PUPPY Balance</h3>
+            <Card style={{width: '18rem'}} className="bg-dark text-white">
+              <Card.Body>
+                <Card.Title>PUPPY Balance</Card.Title>
+                <h4>{this.state.balance}</h4>
+              </Card.Body>
+            </Card>
           </Col>
           <Col sm>
-            <h3>Current Loyalty Level</h3>
+            <Card style={{width: '18rem'}} className="bg-dark text-white">
+              <Card.Body>
+                <Card.Title>Current Loyalty Level</Card.Title>
+                <h4>{this.state.level}</h4>
+              </Card.Body>
+            </Card>
           </Col>
+          <Col></Col>
           <Col sm>
             <h3>Collected</h3>
-            <Carousel>{items}</Carousel>
+            <Carousel responsive={responsive}>{this.state.items}</Carousel>
           </Col>
         </Row>
       </div>
