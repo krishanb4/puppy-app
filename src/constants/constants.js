e56cafabf57836725e912c5f8ad1e3b51c5a97ce
crezix <52518140+crezix@users.noreply.github.com>
2021-05-12 23:24:30 +0530

    nft

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,9 +1,42 @@
 const royaltyCollectibles = {
-  1: {name: 'name', description: 'des', image: 'img'},
-  2: {name: 'name', description: 'des', image: 'img'},
-  3: {name: 'name', description: 'des', image: 'img'},
-  4: {name: 'name', description: 'des', image: 'img'},
+  1: {
+    name: 'Monaliza',
+    description: 'A famous art',
+    image:
+      'https://gateway.pinata.cloud/ipfs/QmVxtQTuo1ycSfhKQA5kgqxyrV68mbbEgYhgerAcjugwRV',
+  },
+  2: {
+    name: 'Monaliza',
+    description: 'A famous art',
+    image:
+      'https://gateway.pinata.cloud/ipfs/QmVxtQTuo1ycSfhKQA5kgqxyrV68mbbEgYhgerAcjugwRV',
+  },
+  3: {
+    name: 'Monaliza',
+    description: 'A famous art',
+    image:
+      'https://gateway.pinata.cloud/ipfs/QmVxtQTuo1ycSfhKQA5kgqxyrV68mbbEgYhgerAcjugwRV',
+  },
+  4: {
+    name: 'Monaliza',
+    description: 'A famous art',
+    image:
+      'https://gateway.pinata.cloud/ipfs/QmVxtQTuo1ycSfhKQA5kgqxyrV68mbbEgYhgerAcjugwRV',
+  },
+  5: {
+    name: 'Monaliza',
+    description: 'A famous art',
+    image:
+      'https://gateway.pinata.cloud/ipfs/QmVxtQTuo1ycSfhKQA5kgqxyrV68mbbEgYhgerAcjugwRV',
+  },
+  6: {
+    name: 'Monaliza',
+    description: 'A famous art',
+    image:
+      'https://gateway.pinata.cloud/ipfs/QmVxtQTuo1ycSfhKQA5kgqxyrV68mbbEgYhgerAcjugwRV',
+  },
 };
-const minterAddress = '';
+const minterAddress = '0x8EF3111271599f7017d104Da20043B987B32Bb1d';
+const puppyAddress = '0xEc6043A64C8eAAbe731Db739e909F0d5254B1392';
 
-export {royaltyCollectibles, minterAddress};
+export {royaltyCollectibles, minterAddress, puppyAddress};
